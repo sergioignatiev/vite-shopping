2e46d12cb27e94fefebc1e82f2c872ed2fb30045
SergioIgnatev <ignatevsergey17@gmail.com>
2024-05-29 20:41:05 +0300

    Updates

--- assets/ParamsView-CMj8-dEY.js
+++
@@ -1,1 +0,0 @@
-import{f as a,c as t,t as s,u as r,o}from"./index-DS3QuMdv.js";const _={__name:"ParamsView",setup(n){const e=a();return(c,u)=>(o(),t("h1",null,s(r(e).params.id),1))}};export{_ as default};
