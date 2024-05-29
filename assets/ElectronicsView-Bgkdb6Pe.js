60b72c2c4ddf9ced87c4d5d1adced010e85f1c0e
SergioIgnatev <ignatevsergey17@gmail.com>
2024-05-30 00:23:49 +0300

    Updates

--- assets/ElectronicsView-Bgkdb6Pe.js
+++
@@ -1,1 +0,0 @@
-import{u as c,k as n,c as r,r as u,F as l,o,t as i}from"./index-CTrd8icq.js";const d={__name:"ElectronicsView",setup(p){const s=c().store,a=n(()=>s.filter(e=>e.category=="electronics"));return(e,m)=>(o(!0),r(l,null,u(a.value,t=>(o(),r("p",{key:t.id},i(t.category),1))),128))}};export{d as default};
